392759e95be0de840df4c11fc811edc8ec3be06d
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-12-03 19:08:42 +0600

    ..

---
+++ dynamic-files/header.php
@@ -0,0 +1,14 @@
+<!-- Dynamic header in html -->
+
+<div class="header-container">
+  <div class="header">
+    <div class="logo-title">
+      <h3>Kothin<span>Train</span></h3>
+      <p>One Stop Solution</p>
+    </div>
+    
+    <div class="logo-img">
+      <img src="" alt="Logo" />
+    </div>
+  </div>
+</div>
