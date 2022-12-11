fc374fdbcfff2aeed291b256845e010a879d209b
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-12-11 21:08:19 +0600

    DOM introduction

--- JavaScript Bootcamp/09 - DOM/intro-DOM.js
+++ JavaScript Bootcamp/09 - DOM/intro-DOM.js
@@ -1,5 +1,13 @@
-// DOM => Document Object Model => link the JS object to the HTML Doc
+// *DOM => Document Object Model => link the JS object to the HTML Doc*
 
 // Selecting Methods => Getting HTML Elements by their Class
 
-const heading = document.getElementsByClassName("title");
+//const heading = document.getElementsByClassName("title");
+//console.log(heading);
+
+// *Selecting Methods => Getting HTML Elements by their ID*
+
+//let dogImg = document.getElementById("dog-img");
+
+// *Selecting Methods => querySelectorAll => It selects all the elements that share that selector*
+let dogTrait = document.querySelectorAll(".item");
